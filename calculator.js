291e2b1926ab53d79eb9bddaa1256c4c9340693e
Prabhat Dongare <dongareprabhat241@gmail.com>
2024-07-19 17:30:37 +0530

    Case: Multiple Numbers

--- calculator.js
+++ calculator.js
@@ -2,9 +2,6 @@
 export function add(numbers) {
     if (numbers === "")
         return 0;
-    else if (numbers.length > 1)
-        return parseInt(numbers[0]) + parseInt(numbers[2])
-    else
-        return parseInt(numbers[0])
-
+    const numArr = numbers.split(",");
+    return numArr.reduce((sum, num) => sum + parseInt(num), 0);
 }
--- test/calculator.test.js
+++ test/calculator.test.js
@@ -9,8 +9,12 @@ test('Need to return the number itself for a single number', () => {
     expect(add("5")).toBe(5);
 });
 
-test('should return the sum of two numbers', () => {
+test('Need to return the sum of two numbers', () => {
     expect(add("1,2")).toBe(3);
     expect(add("1,5")).toBe(6);
 });
 
+test('Need to return the sum of multiple numbers', () => {
+    expect(add("1,2,3")).toBe(6);
+    expect(add("4,5,6,7")).toBe(22);
+});
